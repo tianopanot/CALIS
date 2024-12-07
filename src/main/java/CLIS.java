e8ce0ff711d3f82aebb6ce32148558a7cd4cd0fe
Vhon Michael Tabalan <153250288+Xhyther@users.noreply.github.com>
2024-12-07 21:28:32 +0800

    Initial Commit. Contains the General Layout of the App. Includes the sidebar and the Content Panel

---
+++ src/main/java/CLIS.java
@@ -0,0 +1,10 @@
+//The main fucntion; the entry point or start of the program
+
+public class CLIS {
+    
+    public static void main(String[] args) {
+        MainFrame Frame = new MainFrame();
+        Frame.setVisible(true);
+
+    }
+}
